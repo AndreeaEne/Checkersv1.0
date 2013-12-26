be011b81bf777cc56b144864c6a970e95d57902a
Andreea-Daniela Ene <ene_andreea@ymail.com>
2013-12-26 19:37:27 +0200

    Joc terminat

---
+++ src/Checkers/CheckersMove.java
@@ -0,0 +1,12 @@
+package Checkers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: andreea
+ * Date: 26/12/13
+ * Time: 19:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class CheckersMove {
+    public int srcX, srcY, destX, destY;
+}
